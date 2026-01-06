dd409cef09fd8590ab53b1191592a1e0d369f70f
Christian <christiancamiloriveralamadrid@gmail.com>
2026-01-06 13:11:57 -0500

    starting with layout

---
+++ components/templates/main-layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/organisms/navbar"
+import { Footer } from "@/components/organisms/footer"
+
+export function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  )
+}
